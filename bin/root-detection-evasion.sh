e5c59cdb2b7a9cec7234f94102252c31a89e633b
Daniel Riissanen <daniel.riissanen@nixu.com>
2016-08-02 09:11:23 +0300

    feat: Add usage instructions
    Additional comments:
    - Changed from tabs to 4 spaces
    - Included and made use of the utility script

--- bin/root-detection-evasion.sh
+++ bin/root-detection-evasion.sh
@@ -1,116 +1,163 @@
 #!/bin/bash
 
+set -u
+
 ROOT_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})/.." && pwd)"
-ROOT_DIR="${EXEC_DIR%/}"
-APK_DIR="/home/daniel/Documents/git/android-emulation-environment-setup/apks"
-SDK_DIR="/home/daniel/Downloads/sdk/android-sdk-linux"
-AVD="TestName"
-ADB="${SDK_DIR}/platform-tools/adb"
+ROOT_DIR="${ROOT_DIR%/}"
+source ${ROOT_DIR}/utilities.sh
+
+USAGE="Usage: ./root-detection-evasion.sh <android sdk directory> <avd name>"
+HELP_TEXT="
+OPTIONS
+-s, --silent                Silent mode, suppresses all output except result
+-h, --help                  Display this help and exit
+
+<android sdk directory>     Android SDK installation directory
+<avd name>                  The name of the AVD to launch"
+
+SDK_DIR=""
+AVD=""
+ADB=""
 declare -a EXT_APKS=("DetectionEvader.apk")
 declare -a EXT_PKGS=("com.nixu.substraterootdetectionevasion")
 declare -a APKS=("RootDetector.apk" "RootChecker.apk" "NordeaCodes.apk" "hidesubinary.apk")
 declare -a PKGS=("com.nixu.rootdetection" "com.joeykrim.rootcheck" "com.nordea.mobiletoken" "com.nixu.hideroot")
 
+parse_arguments() {
+    local show_help=0
+    for ((i = 1; i <= $#; i++)); do
+        if [ "${!i}" == "-s" ] || [ "${!i}" == "--silent" ]; then
+            SILENT_MODE=1
+        elif [ "${!i}" == "-h" ] || [ "${!i}" == "--help" ]; then
+            show_help=1
+        elif [ -z "${SDK_DIR}" ]; then
+            SDK_DIR="${!i}"
+        elif [ -z "${AVD}" ]; then
+            AVD="${!i}"
+        else
+            std_err "Unknown argument: ${!i}"
+            std_err "${USAGE}"
+            std_err "See -h for more info"
+            println "${SDK_DIR}"
+            println "${AVD}"
+            exit 1
+        fi
+    done
+
+    if [ ${show_help} -eq 1 ]; then
+        print_help
+        exit
+    fi
+
+    if [ -z "${SDK_DIR}" ] \
+    || [ -z "${AVD}" ]; then
+        std_err "${USAGE}"
+        std_err "See -h for more info"
+        exit 1
+    fi
+
+    SDK_DIR=${SDK_DIR%/}
+} # parse_arguments()
+
 check_files() {
-    if [ ! -d "${APK_DIR}" ]; then
-        printf "APK directory not found!\n"
-        exit 1
-    fi
-
     if [ ! -d "${SDK_DIR}" ]; then
-        printf "Android SDK directory not found!\n"
+        std_err "Android SDK directory not found!"
         exit 1
     fi
 
-    printf "Files are ok\n"
+    ADB="${SDK_DIR}/$(ls ${SDK_DIR})/platform-tools/adb"
+    println "Files are ok"
 } # parse_argumetns()
 
 start_avd() {
-	printf "Starting AVD: ${AVD}\n"
-	${SDK_DIR}/tools/emulator -avd ${AVD} -no-boot-anim -wipe-data -partition-size 2047 & &>/dev/null
+    println "Starting AVD: ${AVD}"
+    ${SDK_DIR}/$(ls ${SDK_DIR})/tools/emulator -avd ${AVD} -no-boot-anim -wipe-data -partition-size 2047 &>/dev/null &
 } # start_avd()
 
 wait_for_device() {
-	printf "Waiting...\n"
-	output=""
-	while [ "${output}" != "stopped" ]; do
-		output="$(${ADB} wait-for-device shell getprop init.svc.bootanim | tr -cd '[[:alpha:]]')"
-		sleep 2
-	done
-	printf "\n"
+    write "Waiting..."
+    output=""
+    while [ "${output}" != "stopped" ]; do
+        # getprop appends a \r at the end of the string and that is why 'tr' is used here
+        output="$(${ADB} wait-for-device shell getprop init.svc.bootanim | tr -cd '[[:alpha:]]')"
+        sleep 2
+        write "."
+    done
+    println "\n"
 } # wait_for_device()
 
 install_root() {
-	printf "ROOTING\n"
+    println "ROOTING"
 
-	printf "Mounting /system as read-write\n"
-	${ADB} shell mount -o rw,remount /system
+    println "Mounting /system as read-write"
+    ${ADB} shell mount -o rw,remount /system
 
-	printf "Transfering \'su\' binary\n"
-	${ADB} push ${ROOT_DIR}/bin/root/su /system/xbin/su
-	${ADB} push ${ROOT_DIR}/bin/root/su /system/xbin/surd
+    println "Transfering \'su\' binary"
+    ${ADB} push ${ROOT_DIR}/bin/root/su /system/xbin/su
+    ${ADB} push ${ROOT_DIR}/bin/root/su /system/xbin/surd
 
-	printf "Changing permission\n"
-	${ADB} shell chmod 06755 /system
-	${ADB} shell chmod 06755 /system/xbin/su
-	${ADB} shell chmod 06755 /system/xbin/surd
+    println "Changing permission"
+    ${ADB} shell chmod 06755 /system
+    ${ADB} shell chmod 06755 /system/xbin/su
+    ${ADB} shell chmod 06755 /system/xbin/surd
 
-	printf "\n"
+    println ""
 } # install_root()
 
 install_substrate() {
-	printf "SUBSTRATE\n"
+    println "SUBSTRATE"
 
-	printf "Uninstalling old Substrate\n"
-	${ADB} shell pm uninstall com.saurik.substrate
-	printf "Installing new Substrate\n"
-	${ADB} install ${ROOT_DIR}/apks/Substrate.apk
-	printf "Linking Substrate files\n"
-	${ADB} shell /data/data/com.saurik.substrate/lib/libSubstrateRun.so do_link
-	printf "Opening Substrate app\n"
-	${ADB} shell am start -n com.saurik.substrate/.SetupActivity
+    println "Uninstalling old Substrate"
+    ${ADB} shell pm uninstall com.saurik.substrate
+    println "Installing new Substrate"
+    ${ADB} install ${ROOT_DIR}/apks/Substrate.apk
+    println "Linking Substrate files"
+    ${ADB} shell /data/data/com.saurik.substrate/lib/libSubstrateRun.so do_link
+    println "Opening Substrate app"
+    ${ADB} shell am start -n com.saurik.substrate/.SetupActivity
 
-	printf "\n"
+    println ""
 } # install_substrate()
 
 install_substrate_extensions() {
-	printf "SUBSTRATE EXTENSIONS\n"
+    println "SUBSTRATE EXTENSIONS\n"
 
-	local app=""
-	local package=""
-	for i in "${#EXTS[@]}"; do
-		app="${EXT_APKS[${i}]}"
-		package="${EXT_PKGS[${i}]}"
-		printf "Uninstalling old apk: ${app}\n"
-		${ADB} shell pm uninstall ${package}
-		printf "Installing new apk: ${app}\n"
-		${ADB} install ${ROOT_DIR}/apks/${app}
-	done
+    local app=""
+    local package=""
+    for i in "${#EXT_APKS[@]}"; do
+        app="${EXT_APKS[${i}]}"
+        package="${EXT_PKGS[${i}]}"
+        println "Uninstalling old apk: ${app}"
+        ${ADB} shell pm uninstall ${package}
+        println "Installing new apk: ${app}"
+        ${ADB} install ${ROOT_DIR}/apks/${app}
+    done
 
-	printf "\n"
+    println ""
 } # install_substrate_extensions()
 
 install_apks() {
-	printf "ADDITIONAL APK'S\n"
-	local app=""
-	local package=""
-	for i in "${#APKS[@]}"; do
-		app="${APKS[${i}]}"
-		package="${PKGS[${i}]}"
-		printf "Uninstalling old apk: ${app}\n"
-		${ADB} shell pm uninstall ${package}
-		printf "Installing new apk: ${app}\n"
-		${ADB} install ${ROOT_DIR}/apks/${app}
-	done
+    println "ADDITIONAL APK\'S"
+    local app=""
+    local package=""
+    for i in "${#APKS[@]}"; do
+        app="${APKS[${i}]}"
+        package="${PKGS[${i}]}"
+        println "Uninstalling old apk: ${app}"
+        ${ADB} shell pm uninstall ${package}
+        println "Installing new apk: ${app}"
+        ${ADB} install ${ROOT_DIR}/apks/${app}
+    done
 
-	printf "\n"
+    println ""
 } # install_apks()
 
 reboot_avd() {
-	printf "Rebooting Android Virtual Device\n"
-	${ADB} shell su -c setprop ctl.restart zygote
+    println "Rebooting Android Virtual Device"
+    ${ADB} shell su -c setprop ctl.restart zygote
 } # reboot()
 
+parse_arguments $@
 check_files
 start_avd
 wait_for_device
@@ -119,3 +166,5 @@ install_substrate
 install_substrate_extensions
 install_apks
 reboot_avd
+wait_for_device
+println "Done!"
